4951496c727187c8f9ed54ac6a5250632a3fc3f6
Rifqi Fachriza <fachriza@Rifqis-MacBook-Pro.local>
2023-12-23 18:58:03 +0700

    read json meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var require: any;
+
+var meta = require('../assets/meta.json');
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularFrontend';
+export class AppComponent implements OnInit {
+  title = meta.title;
+  MetaJson:any;
+  url: string = '/assets/meta.json';
+  
+  constructor() {}
+
+  ngOnInit() {
+    console.log(meta)
+    console.log(this.title)
+  }
 }
